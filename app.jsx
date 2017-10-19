2131840b765d9f9d565818592c6fc8918bfbf995
Abigail <abi25.fly@gmail.com>
2017-10-19 16:55:58 -0500

    first

---
+++ app.jsx
@@ -0,0 +1,104 @@
+
+class Model {
+
+      constructor() {
+            this.todos = [];
+            this.inputValue = null;
+            this.inputValue2 = null;
+            this.render = undefined;
+      }
+
+      subscribe(render) {
+            this.render = render;
+      }
+      inform() {
+            console.log(this.todos.map(e => e.text));
+            this.render();
+      }
+      addTodo(text) {
+            this.todos.push({
+                  id: Utils.uuid(),
+                  text: text,
+                  completed: false
+            });
+            this.inform();
+      }
+      updateTodo(index, todo) {
+            this.todos[index] = todo;
+            this.inform();
+      }
+      removeTodo(todo) {
+            this.todos = this.todos.filter(item => item !== todo);
+            this.inform();
+      }
+     
+}
+
+
+
+
+const App = ({ title, model }) => {
+      const items = model.todos.map((todo, index) => {
+            return (
+
+                  <li key={todo.id}>
+
+                        <div>
+                              <input 
+                                    type="text"
+                                    value={todo.text}
+                                    onChange={e =>
+                                          model.updateTodo(index, {
+                                                id: todo.id,
+                                                text: e.target.value,
+                                                completed: todo.completed
+                                          })}
+                              />
+                            
+                              <button onClick={() => model.removeTodo(todo)}>Eliminar</button>
+                        </div>
+                  </li>
+            );
+      });
+      return (
+            <div className="wrapper">
+
+                  <header>
+                        <h1>Comentarios</h1>
+                        <p> Escribe un comentario:</p>
+                        <form className="registrar"
+                              onSubmit={e => {
+                                    e.preventDefault();
+                                    model.addTodo(model.inputValue);
+                                    model.addTodo(model.inputValue2);
+                              }}
+                        >
+                              <input className="inputInfo" type="text" name="name" placeholder='Nombre' onChange={e => (model.inputValue = e.target.value)} />
+                              <input className="inputInfo" type="text" name="name" placeholder='Comentario' onChange={e => (model.inputValue2 = e.target.value)} />
+                              <button type="submit" name="submit" value="submit">Enviar</button>
+                        </form>
+
+                  </header>
+
+                  <div class="main">
+                        <h2>Comentarios</h2>
+                        <ul>{items}</ul>
+                  </div>
+            </div>
+
+      );
+};
+
+let model = new Model();
+let counter = 1;
+let render = () => {
+      console.log('render times: ', counter++);
+      ReactDOM.render(
+            <App title="Comentarios" model={model} />,
+            document.getElementById('container')
+      );
+};
+
+
+model.subscribe(render);
+render(); 
